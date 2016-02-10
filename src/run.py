c17dbf469ed4d769b1a40ddb512013146b230572
Jens Kadenbach <gitdax@daxbau.net>
2016-02-10 11:12:50 +0100

    Handle sigints properly

--- src/run.py
+++ src/run.py
@@ -1,4 +1,20 @@
+from tornado.ioloop import IOLoop
+from tornado.platform.asyncio import AsyncIOMainLoop
+
 from blockserver import server
+from tornado import options
+import signal
+
+
+def sigint_handler(sig, frame):
+    asyncio_loop = AsyncIOMainLoop.current()
+    asyncio_loop.add_callback_from_signal(asyncio_loop.stop)
+    io_loop = IOLoop.current()
+    io_loop.add_callback_from_signal(io_loop.stop)
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == "__main__":
+    options.parse_command_line()
     server.main()
+
