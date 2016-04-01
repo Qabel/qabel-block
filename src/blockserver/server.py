178ed3b0813b053f77c171d0dc1b0d8fb3507874
Jens Kadenbach <gitdax@daxbau.net>
2016-04-01 13:28:53 +0200

    Enable xheader support
    See http://www.tornadoweb.org/en/stable/httpserver.html
    It is just a config option in the HttpServer

--- src/blockserver/server.py
+++ src/blockserver/server.py
@@ -304,7 +304,8 @@ def main():
     if options.debug:
         application.listen(address=options.address, port=options.port)
     else:
-        server = tornado.httpserver.HTTPServer(application)
+        server = tornado.httpserver.HTTPServer(application,
+                                               xheaders=True)
         server.bind(options.port)
         server.start()
     if options.asyncio:
