5d80b3b54506e0b404bf4f5a91ba82da5a1a19b8
Marian Beermann <beermann@qabel.de>
2016-06-06 12:13:16 +0200

    Configuration from uWSGI, graceful worker destruction

--- src/uwsgi_plumbing.py
+++ src/uwsgi_plumbing.py
@@ -2,8 +2,9 @@
 Plumbing to bolt tornado onto uWSGI while retaining asynchronous request handling.
 
 It takes the same options as the run.py script, but here these are passed with the --pyargv uWSGI option.
+Alternatively you can set options in the uWSGI worker configuration by prefixing them with "block_".
 
-The --prometheus-port option is the _base_ port. Each worker uses it's own port (prometheus_port + worker_id).
+The prometheus-port option is the _base_ port. Each worker uses it's own port (prometheus_port + worker_id).
 
 Examples:
 
@@ -30,12 +31,17 @@ plugin=python
 master=true
 processes=4
 python-worker-override=uwsgi_plumbing.py
-pyargv=--dummy --debug --dummy-auth=MAGICFAIRY
+
+# You can mix _ and - as you like
+block_dummy=true
+block_debug=true
+block_dummy_auth=MAGICFAIRY
 $ uwsgi uwsgi-block.ini
 """
 
 import json
 import logging.config
+import signal
 import socket
 import sys
 
@@ -50,7 +56,7 @@ from prometheus_client import start_http_server
 from blockserver.server import make_app
 
 
-def init(fd):
+def spawn_on_socket(fd):
     worker_id = uwsgi.worker_id()
     application = make_app(debug=options.debug)
     server = HTTPServer(application, xheaders=True)
@@ -64,21 +70,52 @@ def init(fd):
     print('uwsgi plumber reporting for duty on uWSGI worker %s' % worker_id)
 
 
-# Parse command line
-if len(sys.argv) == 2 and not sys.argv[1].startswith('--'):
-    options.parse_config_file(sys.argv[1])
-else:
-    options.parse_command_line()
+def stop_ioloop(sig, frame):
+    loop = IOLoop.current()
+    loop.add_callback_from_signal(loop.stop)
 
-with open(options.logging_config, 'r') as conf:
-    conf_dictionary = json.load(conf)
-    logging.config.dictConfig(conf_dictionary)
+
+def apply_config_dict(config_dict, prefix=''):
+    for name, value in config_dict.items():
+        if '_' in name:
+            name = name.replace('_', '-')
+        if not name.startswith(prefix):
+            continue
+        name = name[len(prefix):]
+        if name not in options:
+            print('Invalid block_ server option in uWSGI config file: {}{}'.format(prefix, name))
+            sys.exit(1)
+        options._options[name].parse(value.decode())
+
+
+def parse_arguments(argv):
+    if len(argv) == 2 and not argv[1].startswith('--'):
+        options.parse_config_file(argv[1])
+    else:
+        options.parse_command_line()
+
+
+def configure_logging():
+    with open(options.logging_config, 'r') as conf:
+        conf_dictionary = json.load(conf)
+        logging.config.dictConfig(conf_dictionary)
+
+# Parse configuration from uWSGI config
+apply_config_dict(uwsgi.opt, prefix='block-')
+# Parse command line as well (from --pyargv)
+# Overrides configuration file
+parse_arguments(sys.argv)
+
+configure_logging()
 
 # TODO: handle signals
-# SIGINT = destroy
 # SIGHUP = graceful reload
+signal.signal(signal.SIGINT, stop_ioloop)
 
 # spawn a handler for every uWSGI socket
 for fd in uwsgi.sockets:
-    init(fd)
-IOLoop.current().start()
+    spawn_on_socket(fd)
+loop = IOLoop.current()
+loop.set_blocking_log_threshold(1)
+loop.start()
+print('Worker %s dead.' % uwsgi.worker_id())
